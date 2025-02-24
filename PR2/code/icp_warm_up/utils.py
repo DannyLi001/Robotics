23e235979343107cbb40c3374968935829922219
DannyLi001 <102645918+DannyLi001@users.noreply.github.com>
2025-02-24 15:07:49 -0800

    Add files via upload

--- PR2/code/icp_warm_up/utils.py
+++ PR2/code/icp_warm_up/utils.py
@@ -32,22 +32,24 @@ def load_pc(model_name, id):
 
 
 def visualize_icp_result(source_pc, target_pc, pose):
-  '''
-  Visualize the result of ICP
-  source_pc: numpy array, (N, 3)
-  target_pc: numpy array, (N, 3)
-  pose: SE(4) numpy array, (4, 4)
-  '''
-  source_pcd = o3d.geometry.PointCloud()
-  source_pcd.points = o3d.utility.Vector3dVector(source_pc.reshape(-1, 3))
-  source_pcd.paint_uniform_color([0, 0, 1])
+    '''
+    Visualize the result of ICP with a coordinate axis.
+    source_pc: numpy array, (N, 3)
+    target_pc: numpy array, (N, 3)
+    pose: SE(4) numpy array, (4, 4)
+    '''
+    source_pcd = o3d.geometry.PointCloud()
+    source_pcd.points = o3d.utility.Vector3dVector(source_pc.reshape(-1, 3))
+    source_pcd.paint_uniform_color([0, 0, 1])  # Blue for source
 
-  target_pcd = o3d.geometry.PointCloud()
-  target_pcd.points = o3d.utility.Vector3dVector(target_pc.reshape(-1, 3))
-  target_pcd.paint_uniform_color([1, 0, 0])
+    target_pcd = o3d.geometry.PointCloud()
+    target_pcd.points = o3d.utility.Vector3dVector(target_pc.reshape(-1, 3))
+    target_pcd.paint_uniform_color([1, 0, 0])  # Red for target
 
-  source_pcd.transform(pose)
+    target_pcd.transform(pose)
 
-  o3d.visualization.draw_geometries([source_pcd, target_pcd])
+    # Create a coordinate frame (axis) at the origin
+    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
 
+    o3d.visualization.draw_geometries([source_pcd, target_pcd, axis])
 
